6c48168f531aa82fb8d1fe9622362aaa63d4b1cb
HH <1299369169@qq.com>
2023-11-20 17:55:21 +0800

    第四版，多线程导出，发现速度变慢了，4线程472s，8线程460s I/O密集型任务，大数据写入写出，线程处理 CPU密集型任务，大数据计算，图计算，进程处理

--- BI_download/mysql2csv_test1.py
+++ BI_download/mysql2csv_test1.py
@@ -3,11 +3,12 @@ from mysql.connector import Error
 import time
 import os
 import traceback
-
+import concurrent.futures
+import pandas as pd
 
 def read_mysql_info_from_file(file_path):
     try:
-        with open(file_path, 'r',encoding='utf8') as file:
+        with open(file_path, 'r', encoding='utf8') as file:
             lines = file.readlines()
             # print(lines)
 
@@ -30,7 +31,12 @@ def read_mysql_info_from_file(file_path):
         return None
 
 
-def export_mysql_data_batch(host, user, password, database, table, output_file, max_rows_per_sheet=1000000):
+def export_mysql_data_chunk(host, user, password, database, table, header_info, rows, output_file):
+    df = pd.DataFrame(rows, columns=header_info)
+    with open(output_file, 'a', encoding='utf-8') as file:
+        df.to_csv(file, index=False, header=False)
+
+def export_mysql_data_batch_threaded(host, user, password, database, table, output_file, max_rows_per_sheet=1000000, num_threads=4):
     start_time = time.time()
     total_rows_exported = 0
 
@@ -47,20 +53,11 @@ def export_mysql_data_batch(host, user, password, database, table, output_file, 
 
             cursor = connection.cursor()
 
-            # 获取表头信息
             cursor.execute(f"DESCRIBE {table}")
             header_info = [column[0] for column in cursor.fetchall()]
 
             query = f"SELECT * FROM {database}.{table};"
             cursor.execute(query)
-
-            # 使用生成器逐行获取数据
-            def generate_rows():
-                while True:
-                    rows = cursor.fetchmany(max_rows_per_sheet)
-                    if not rows:
-                        break
-                    yield rows
 
             current_sheet = 1
             current_row_count = 0
@@ -68,36 +65,30 @@ def export_mysql_data_batch(host, user, password, database, table, output_file, 
             if not os.path.exists(output_directory):
                 os.makedirs(output_directory)
 
-            file_path = os.path.join(output_directory, f"{output_file}_sheet{current_sheet}.csv")
+            with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+                futures = []
 
-            with open(file_path, 'w', encoding='utf-8') as file:
-                # 写入标题
-                file.write(','.join(header_info) + '\n')
+                for rows in generate_rows(cursor, max_rows_per_sheet):
+                    file_path = os.path.join(output_directory, f"{output_file}_sheet{current_sheet}.csv")
+                    future = executor.submit(export_mysql_data_chunk, host, user, password, database, table, header_info, rows, file_path)
+                    futures.append(future)
 
-                for rows in generate_rows():
-                    for row in rows:
-                        file.write(','.join(map(str, row)) + '\n')
-                        current_row_count += 1
-                        total_rows_exported += 1
-                        # 每次达到最大行数时，切换到新的 sheet
-                        if current_row_count >= max_rows_per_sheet:
-                            file.close()
-                            current_sheet += 1
-                            print(f'切换{current_sheet}')
-                            current_row_count = 0
-                            file_path = os.path.join(output_directory, f"{output_file}_sheet{current_sheet}.csv")
-                            file = open(file_path, 'w', encoding='utf-8')
-                            # 写入标题到新的 sheet
-                            file.write(','.join(header_info) + '\n')
+                    current_row_count += len(rows)
+                    total_rows_exported += len(rows)
 
-                    print(f"{output_file}_sheet{current_sheet-1}.csv导出成功")
+                    if current_row_count >= max_rows_per_sheet:
+                        current_sheet += 1
+                        current_row_count = 0
+                    print(f"{output_file}_sheet{current_sheet - 1}.csv导出成功")
+
+                # 等待所有线程完成
+                concurrent.futures.wait(futures)
 
             print(f"数据已成功导出到 {output_directory}.{output_file}")
 
     except Error as e:
         print(f"错误: {e}")
         traceback.print_exc()
-
 
     finally:
         if connection.is_connected():
@@ -106,28 +97,26 @@ def export_mysql_data_batch(host, user, password, database, table, output_file, 
             print("连接已关闭")
 
     end_time = time.time()
-
     print(f'导出总数: {total_rows_exported} 条数据')
     print(f'导出时间: {end_time - start_time}秒')
 
-
-
+def generate_rows(cursor, max_rows_per_sheet):
+    while True:
+        rows = cursor.fetchmany(max_rows_per_sheet)
+        if not rows:
+            break
+        yield rows
 
 # 从文件中读取数据库信息
 file_path = "database_info.txt"  # 替换成你的文件路径
 db_info = read_mysql_info_from_file(file_path)
 
 if __name__ == '__main__':
-
     if db_info:
-        # 解包数据库信息
         host, user, password, database, table, output_file = db_info
-
-        # 设置每个 sheet 的最大行数
         max_rows_per_sheet = 1000000
-
-        # 调用导出函数
-        export_mysql_data_batch(host, user, password, database, table, output_file, max_rows_per_sheet)
+        num_threads = 8 # 调整线程数
+        export_mysql_data_batch_threaded(host, user, password, database, table, output_file, max_rows_per_sheet, num_threads)
         time.sleep(10)
     else:
         print("读取数据库信息失败，请检查文件。")
