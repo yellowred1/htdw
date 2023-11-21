fe000ed5f5b200469cc6eaaf2370117a50b53186
HH <1299369169@qq.com>
2023-11-21 11:08:03 +0800

    终版：导出690万条数据209s的时间 支持自定义导出sql数据

--- BI_download/mysql2csv_test1.py
+++ BI_download/mysql2csv_test1.py
@@ -2,14 +2,12 @@ import mysql.connector
 from mysql.connector import Error
 import time
 import os
-import traceback
 
 
 def read_mysql_info_from_file(file_path):
     try:
-        with open(file_path, 'r',encoding='utf8') as file:
+        with open(file_path, 'r', encoding='utf8') as file:
             lines = file.readlines()
-            # print(lines)
 
             # 初始化字典
             variables = {}
@@ -20,18 +18,23 @@ def read_mysql_info_from_file(file_path):
                     var_name, var_value = map(str.strip, line.split('='))
                     variables[var_name] = var_value.strip('"')
                 except Exception as e:
-                    print(f"处理行时出错: {e}")
+                    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), f"处理行时出错: {e}")
 
             return variables.get('host'), variables.get('user'), variables.get('password'), variables.get(
                 'database'), variables.get('table'), variables.get('output_file')
 
     except Exception as e:
-        print(f"读取文件错误: {e}")
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), f"读取文件错误: {e}")
         return None
+
+
+def get_query_header_info(cursor):
+    return [column[0] for column in cursor.description]
 
 
 def export_mysql_data_batch(host, user, password, database, table, output_file, max_rows_per_sheet=1000000):
     start_time = time.time()
+
     total_rows_exported = 0
 
     try:
@@ -43,16 +46,37 @@ def export_mysql_data_batch(host, user, password, database, table, output_file, 
         )
 
         if connection.is_connected():
-            print("成功连接到数据库")
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "成功连接到数据库")
 
             cursor = connection.cursor()
 
-            # 获取表头信息
-            cursor.execute(f"DESCRIBE {table}")
-            header_info = [column[0] for column in cursor.fetchall()]
+            # # 获取表头信息
+            # cursor.execute(f"DESCRIBE {table}")
+            # header_info = [column[0] for column in cursor.fetchall()]
 
-            query = f"SELECT * FROM {database}.{table};"
-            cursor.execute(query)
+            table_name = input("是否自定义导出表数据（是/否）：")
+            if table_name == '是':
+                print("请输入SQL语句，以';'结束。多行输入完成后按两次回车：")
+                sql_lines = []
+                while True:
+                    line = input()
+                    if line.strip() == '':
+                        break
+                    sql_lines.append(line)
+
+                sql = ' '.join(sql_lines)
+                print(sql)
+                cursor.execute(sql)
+            elif table_name == '否':
+                query = f"SELECT * FROM {database}.{table};"
+                cursor.execute(query)
+            else:
+                print('输入有误！！')
+                pass
+
+
+            # 获取查询结果的表头信息
+            header_info = get_query_header_info(cursor)
 
             # 使用生成器逐行获取数据
             def generate_rows():
@@ -69,6 +93,7 @@ def export_mysql_data_batch(host, user, password, database, table, output_file, 
                 os.makedirs(output_directory)
 
             file_path = os.path.join(output_directory, f"{output_file}_sheet{current_sheet}.csv")
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), f'写入{file_path}')
 
             with open(file_path, 'w', encoding='utf-8') as file:
                 # 写入标题
@@ -81,23 +106,25 @@ def export_mysql_data_batch(host, user, password, database, table, output_file, 
                         total_rows_exported += 1
                         # 每次达到最大行数时，切换到新的 sheet
                         if current_row_count >= max_rows_per_sheet:
-                            print(f'{current_sheet}')
                             file.close()
                             current_sheet += 1
+
                             current_row_count = 0
                             file_path = os.path.join(output_directory, f"{output_file}_sheet{current_sheet}.csv")
                             file = open(file_path, 'w', encoding='utf-8')
                             # 写入标题到新的 sheet
-                            with open(file_path, 'w', encoding='utf-8') as new_file:
-                                new_file.write(','.join(header_info) + '\n')
+                            file.write(','.join(header_info) + '\n')
 
-                    print(f"{output_file}_sheet{current_sheet-1}.csv导出成功")
-
-            print(f"数据已成功导出到 {output_directory}.{output_file}")
+                    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                          f"{output_file}_sheet{current_sheet - 1}.csv导出成功")
+                    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), f'切换sheet{current_sheet}')
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                  f"{output_file}_sheet{current_sheet}.csv导出成功")
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                  f'数据已成功导出到 {output_directory}.{output_file}集合')
 
     except Error as e:
-        print(f"错误: {e}")
-        traceback.print_exc()
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), f"错误: {e}")
 
 
     finally:
@@ -110,8 +137,6 @@ def export_mysql_data_batch(host, user, password, database, table, output_file, 
 
     print(f'导出总数: {total_rows_exported} 条数据')
     print(f'导出时间: {end_time - start_time}秒')
-
-
 
 
 # 从文件中读取数据库信息
@@ -127,8 +152,10 @@ if __name__ == '__main__':
         # 设置每个 sheet 的最大行数
         max_rows_per_sheet = 1000000
 
+        print("*注意* 每一个CSV文件保存100万条数")
+
         # 调用导出函数
         export_mysql_data_batch(host, user, password, database, table, output_file, max_rows_per_sheet)
         time.sleep(10)
     else:
-        print("读取数据库信息失败，请检查文件。")
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "读取数据库信息失败，请检查文件。")
